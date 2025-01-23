af035895151dc1ef591765c09d0f39489c491f93
UnatxChicano <unatx.chicanore@elorrieta-errakamri.com>
2025-01-23 10:54:32 +0100

    Relaciones Agencia y Viajes echas.

--- Grupo2Reto2/src/Modelo/Agencia.java
+++ Grupo2Reto2/src/Modelo/Agencia.java
@@ -1,5 +1,6 @@
 package Modelo;
 
+import java.util.ArrayList;
 
 public class Agencia {
 
@@ -8,6 +9,7 @@ public class Agencia {
 	private String colorMarca;
 	private String numeroEmpleados;
 	private String logo;
+	private ArrayList<Viaje>viajes;
 	
 	public Agencia() {
 		idAgencia="";
@@ -15,55 +17,75 @@ public class Agencia {
 		colorMarca="";
 		numeroEmpleados="";
 		logo="";
+		ArrayList<Viaje> viajes = new ArrayList<Viaje>();
 	}
 	
-	public Agencia(String idAgencia, String nombre, String colorMarca, String numeroEmpleados, String logo) {
+	public Agencia(String idAgencia, String nombre, String colorMarca, String numeroEmpleados, String logo, ArrayList<Viaje> viajes) {
 		
 		this.idAgencia=idAgencia;
 		this.nombre=nombre;
 		this.colorMarca=colorMarca;
 		this.numeroEmpleados=numeroEmpleados;
 		this.logo=logo;
+		this.viajes=viajes;
 			
 	}
-	
 
-	
 	public String getIdAgencia() {
 		return idAgencia;
 	}
+
 	public void setIdAgencia(String idAgencia) {
 		this.idAgencia = idAgencia;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getColorMarca() {
 		return colorMarca;
 	}
+
 	public void setColorMarca(String colorMarca) {
 		this.colorMarca = colorMarca;
 	}
+
 	public String getNumeroEmpleados() {
 		return numeroEmpleados;
 	}
-	public void setNumeroEmpleados(String numeroEmpleado) {
-		this.numeroEmpleados = numeroEmpleado;
+
+	public void setNumeroEmpleados(String numeroEmpleados) {
+		this.numeroEmpleados = numeroEmpleados;
 	}
+
 	public String getLogo() {
 		return logo;
 	}
+
 	public void setLogo(String logo) {
 		this.logo = logo;
+	}
+
+	public ArrayList<Viaje> getViajes() {
+		return viajes;
+	}
+
+	public void setViajes(ArrayList<Viaje> viajes) {
+		this.viajes = viajes;
 	}
 
 	@Override
 	public String toString() {
 		return "Agencia [idAgencia=" + idAgencia + ", nombre=" + nombre + ", colorMarca=" + colorMarca
-				+ ", numeroEmpleados=" + numeroEmpleados + ", logo=" + logo + "]";
+				+ ", numeroEmpleados=" + numeroEmpleados + ", logo=" + logo + ", viajes=" + viajes + "]";
 	}
+	
+	
+
 	
 }
--- Grupo2Reto2/src/Modelo/Viaje.java
+++ Grupo2Reto2/src/Modelo/Viaje.java
@@ -1,88 +1,125 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Viaje {
 	
 	
 	private String idViaje;
-	private String nombre;
+	private String nombreViaje;
 	private String descripcion;
 	private String tipoViaje;
 	private String fechas;
 	private String duracion;
 	private String descServicio;
+	private Agencia agencia;
+	private Pais pais;
+	private ArrayList<Evento> evento;
+	
+
 	
 	
 	Viaje (){
 		idViaje="";
-		nombre="";
+		nombreViaje="";
 		descripcion="";
 		tipoViaje="";
 		fechas="";
 		duracion="";
 		descServicio="";
+		agencia = new Agencia();
+		pais=new Pais();
+		ArrayList<Evento> evento = new ArrayList<Evento>();
 	}
 	
-	Viaje(String idViaje, String nombre, String descripcion, String tipoViaje, String fechas, String duracion, String descServicio){
+	Viaje(String idViaje, String nombreViaje, String descripcion, String tipoViaje, String fechas, String duracion, String descServicio, Agencia agencia, Pais pais, ArrayList<Evento> evento){
 		
 		this.idViaje=idViaje;
-		this.nombre=nombre;
+		this.nombreViaje=nombreViaje;
 		this.descripcion=descripcion;
 		this.tipoViaje=tipoViaje;
 		this.fechas=fechas;
 		this.duracion=duracion;
 		this.descServicio=descServicio;
-		
+		this.agencia= agencia;
+		this.pais=pais;
+		this.evento=evento;
 		
 	}
-	
-	
+
 	public String getIdViaje() {
 		return idViaje;
 	}
+
 	public void setIdViaje(String idViaje) {
 		this.idViaje = idViaje;
 	}
-	public String getNombre() {
-		return nombre;
+
+	public String getNombreViaje() {
+		return nombreViaje;
 	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+
+	public void setNombreViaje(String nombreViaje) {
+		this.nombreViaje = nombreViaje;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	public String getTipoViaej() {
+
+	public String getTipoViaje() {
 		return tipoViaje;
 	}
-	public void setTipoViaej(String tipoViaej) {
-		this.tipoViaje = tipoViaej;
+
+	public void setTipoViaje(String tipoViaje) {
+		this.tipoViaje = tipoViaje;
 	}
+
 	public String getFechas() {
 		return fechas;
 	}
+
 	public void setFechas(String fechas) {
 		this.fechas = fechas;
 	}
+
 	public String getDuracion() {
 		return duracion;
 	}
+
 	public void setDuracion(String duracion) {
 		this.duracion = duracion;
 	}
+
 	public String getDescServicio() {
 		return descServicio;
 	}
+
 	public void setDescServicio(String descServicio) {
 		this.descServicio = descServicio;
 	}
 
-	@Override
-	public String toString() {
-		return "Viaje [idViaje=" + idViaje + ", nombre=" + nombre + ", descripcion=" + descripcion + ", tipoViaje="
-				+ tipoViaje + ", fechas=" + fechas + ", duracion=" + duracion + ", descServicio=" + descServicio + "]";
+	public Agencia getAgencia() {
+		return agencia;
 	}
 
+	public void setAgencia(Agencia agencia) {
+		this.agencia = agencia;
+	}
+
+	@Override
+	public String toString() {
+		return "Viaje [idViaje=" + idViaje + ", nombreViaje=" + nombreViaje + ", descripcion=" + descripcion
+				+ ", tipoViaje=" + tipoViaje + ", fechas=" + fechas + ", duracion=" + duracion + ", descServicio="
+				+ descServicio + ", agencia=" + agencia + "]";
+	}
+
+	
+	
+	
 }
+	
