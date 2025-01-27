f9b4e55cdec65c7f8de96309add900a6c286cddb
UnatxChicano <unatx.chicanore@elorrieta-errakamri.com>
2025-01-27 10:13:33 +0100

    Correccion clase vuelo

--- Grupo2Reto2/src/Modelo/Vuelo.java
+++ Grupo2Reto2/src/Modelo/Vuelo.java
@@ -9,7 +9,7 @@ public class Vuelo {
 	private String codigoVuelo;
 	private String hora;
 	private String duracion;
-	private String vueloIda;
+	private Vuelo vueloIda;
 	private Viaje viaje;
 	private Aeropuerto aeropuertoOrigen;
 	private Aeropuerto aeropuertoDestino;
@@ -23,16 +23,16 @@ public class Vuelo {
 		codigoVuelo="";
 		hora="";
 		duracion="";
-		vueloIda="";
+		vueloIda =new Vuelo();
 		viaje = new Viaje();
 		aeropuertoOrigen= new Aeropuerto();
 		aeropuertoDestino= new Aeropuerto();
-		aerolinea = new Aerolinea();
+		aerolinea = new Aerolinea(); 
 		
 	}
 
 	
-	Vuelo(String idEvento, String nombreEvento, String precio,String fecha, String codigoVuelo, String hora,String duracion,String vueloIda,Viaje viaje ,Aeropuerto aeropuertoOrigen, Aeropuerto aeropuertoDestino,Aerolinea aerolinea){
+	Vuelo(String idEvento, String nombreEvento, String precio,String fecha, String codigoVuelo, String hora,String duracion,Vuelo vueloIda,Viaje viaje ,Aeropuerto aeropuertoOrigen, Aeropuerto aeropuertoDestino,Aerolinea aerolinea){
 		
 		this.idEvento=idEvento;
 		this.nombreEvento=nombreEvento;
@@ -49,101 +49,127 @@ public class Vuelo {
 
 	}
 
+	
+
 	public String getIdEvento() {
 		return idEvento;
 	}
+
 
 	public void setIdEvento(String idEvento) {
 		this.idEvento = idEvento;
 	}
 
+
 	public String getNombreEvento() {
 		return nombreEvento;
 	}
+
 
 	public void setNombreEvento(String nombreEvento) {
 		this.nombreEvento = nombreEvento;
 	}
 
+
 	public String getPrecio() {
 		return precio;
 	}
+
 
 	public void setPrecio(String precio) {
 		this.precio = precio;
 	}
 
+
 	public String getFecha() {
 		return fecha;
 	}
+
 
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
 
+
 	public String getCodigoVuelo() {
 		return codigoVuelo;
 	}
+
 
 	public void setCodigoVuelo(String codigoVuelo) {
 		this.codigoVuelo = codigoVuelo;
 	}
 
+
 	public String getHora() {
 		return hora;
 	}
+
 
 	public void setHora(String hora) {
 		this.hora = hora;
 	}
 
+
 	public String getDuracion() {
 		return duracion;
 	}
+
 
 	public void setDuracion(String duracion) {
 		this.duracion = duracion;
 	}
 
-	public String getVueloIda() {
+
+	public Vuelo getVueloIda() {
 		return vueloIda;
 	}
 
-	public void setVueloIda(String vueloIda) {
+
+	public void setVueloIda(Vuelo vueloIda) {
 		this.vueloIda = vueloIda;
 	}
+
 
 	public Viaje getViaje() {
 		return viaje;
 	}
 
+
 	public void setViaje(Viaje viaje) {
 		this.viaje = viaje;
 	}
+
 
 	public Aeropuerto getAeropuertoOrigen() {
 		return aeropuertoOrigen;
 	}
 
+
 	public void setAeropuertoOrigen(Aeropuerto aeropuertoOrigen) {
 		this.aeropuertoOrigen = aeropuertoOrigen;
 	}
+
 
 	public Aeropuerto getAeropuertoDestino() {
 		return aeropuertoDestino;
 	}
 
+
 	public void setAeropuertoDestino(Aeropuerto aeropuertoDestino) {
 		this.aeropuertoDestino = aeropuertoDestino;
 	}
+
 
 	public Aerolinea getAerolinea() {
 		return aerolinea;
 	}
 
+
 	public void setAerolinea(Aerolinea aerolinea) {
 		this.aerolinea = aerolinea;
 	}
+
 
 	@Override
 	public String toString() {
@@ -154,7 +180,5 @@ public class Vuelo {
 	}
 
 	
-	
-
 
 }
